e9eeb89ef67f9a102ef4a45d7a03d5ffc03f61fb
†ZeroCool† <herocool263@gmail.com>
2024-08-16 01:38:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,23 +6,23 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "davidcyril209@gmail.com";
-global.location = "Abuja, Nigeria";
+global.email = "pendama99@gmail.com";
+global.location = "Harare, Zimbabwe";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
-global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
-global.github = process.env.GITHUB || "https://github.com/DeeCeeXxx/Queen_Anita-V2";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
-global.devs = "https://t.me/deecee_x";
-global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Harare";
+global.github = process.env.GITHUB || "https://github.com/ZeroCool263/Queen_Anita-V2_BetaV1";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaZypgoIyPtaodVKp61o";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaZypgoIyPtaodVKp61o";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/5ed6afd2ae3843d0cd336dfa23f9a8c7.jpg";
+global.devs = "https://t.me/darkzerocool";
+global.sudo = process.env.SUDO || "233599662889";
+global.owner = process.env.OWNER_NUMBER || "263712473551";
 global.style = process.env.STYLE || "3";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
@@ -48,11 +48,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`QUEEN_ANITA-V2™`",
-  author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
-  packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  caption: process.env.CAPTION || "`DARK_ZERO-COOL™`",
+  author: process.env.PACK_AUTHER || "DARK_ZERO-COOL",
+  packname: process.env.PACK_NAME || "Z E R O C",
+  botname: process.env.BOT_NAME || "DARK_ZERO-COOL",
+  ownername: process.env.OWNER_NAME || "DARKZEROCOOL",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -64,7 +64,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "A N I T A").toUpperCase(),
+  LANG: (process.env.THEME || "D A R K").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
